f496933bed1f8f2310c66cc4ddfd5d45747bd0ea
Ivan <izzyumkin@gmail.com>
2021-03-09 13:42:44 +0500

    Initial Commit

---
+++ Music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Иван Изюмкин on 09.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
